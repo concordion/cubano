cd303f7b28ea14ad54db7ee6d8c71d06da2dd2bd
Andrew Sumner <andrew.sumner@xtra.co.nz>
2018-08-18 18:01:28 +1200

    Add logging support for Proxy-Vole

--- cubano-httpeasy/src/test/java/org/concordion/cubano/driver/http/HttpEasyReaderTests.java
+++ cubano-httpeasy/src/test/java/org/concordion/cubano/driver/http/HttpEasyReaderTests.java
@@ -6,11 +6,19 @@ import static org.junit.Assert.assertThat;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 
 public class HttpEasyReaderTests {
 
     @Test
     public void httpEasyRequest() throws HttpResponseException, IOException {
+        // As there are no Logback configuration files in this project we're not getting the TRACE level logs.
+        // This has been done solely to view the ProxyVole log output
+        Logger logger = (Logger) LoggerFactory.getILoggerFactory().getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.setLevel(Level.ALL);
         
         HttpEasy.withDefaults().proxyConfiguration(ProxyConfiguration.AUTOMATIC);
 
