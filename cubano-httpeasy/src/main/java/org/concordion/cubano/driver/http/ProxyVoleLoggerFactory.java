6fefcdd6664c1f003fd82ed679b9c761c61ff6eb
Andrew Sumner <andrew.sumner@xtra.co.nz>
2018-08-28 22:41:08 +1200

    Added comments explaining refactor

--- cubano-httpeasy/src/main/java/org/concordion/cubano/driver/http/ProxyVoleLoggerFactory.java
+++ cubano-httpeasy/src/main/java/org/concordion/cubano/driver/http/ProxyVoleLoggerFactory.java
@@ -5,6 +5,8 @@ import com.github.markusbernhardt.proxy.util.Logger.LogBackEnd;
 public class ProxyVoleLoggerFactory {
 
     public static LogBackEnd getBackendLogger() {
+        // Has been extracted into this method to prevent ClassNotFound exception when ProxyVole dependency has been excluded.
+        // For some reason having "new ProxyVoleLogger()" in HttpEasyDefaults class caused problems at runtime.
         return new ProxyVoleLogger();
     }
 
