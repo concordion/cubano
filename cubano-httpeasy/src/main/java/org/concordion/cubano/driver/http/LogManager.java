2d898e18cc98f62ff83a34055797d1782463e1a3
jkemp002 <jkemp002@P1130051191.corp.ssi.govt.nz>
2018-12-07 11:35:11 +1300

    Refactor LogManager to allow buffering of request and response messages. Fix RawDataWriter payload message to be a String rather than bytes (for logging)

--- cubano-httpeasy/src/main/java/org/concordion/cubano/driver/http/LogManager.java
+++ cubano-httpeasy/src/main/java/org/concordion/cubano/driver/http/LogManager.java
@@ -4,6 +4,7 @@ public class LogManager {
     private LogWriter logWriter;
     private boolean logRequest;
     private boolean logRequestDetails;
+    private StringBuilder buffer = new StringBuilder();
     
     public LogManager(LogWriter logWriter, boolean logRequestDetails) {
         this.logRequest = HttpEasyDefaults.getLogRequest() || logRequestDetails;
@@ -22,12 +23,32 @@ public class LogManager {
         }
     }
 
+    public void buffer(String msg, Object... args) {
+        buffer.append(logWriter.getFormattedMessage(msg, args));
+    }
+
+    public void flushRequest() {
+        if (logRequestDetails && buffer.length() > 0) {
+            logWriter.request(buffer.toString());
+        }
+
+        buffer = new StringBuilder();
+    }
+
     public void request(String msg, Object... args) {
         if (logRequestDetails) {
             logWriter.request(msg, args);
         }
     }
     
+    public void flushResponse() {
+        if (logRequestDetails && buffer.length() > 0) {
+            logWriter.response(buffer.toString());
+        }
+
+        buffer = new StringBuilder();
+    }
+
     public void response(String msg, Object... args) {
         if (logRequestDetails) {
             logWriter.response(msg, args);
