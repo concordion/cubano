d709e3064ef17ab261ae9c666e5710f5490db1bb
Andrew Sumner <andrew.sumner@xtra.co.nz>
2019-09-22 01:26:53 +1200

    Keep logwriter in original package for backwards compatibility

---
+++ cubano-httpeasy/src/main/java/org/concordion/cubano/driver/http/LogWriter.java
@@ -0,0 +1,21 @@
+package org.concordion.cubano.driver.http;
+
+/**
+ * An interface for proving custom logging output.
+ */
+public abstract class LogWriter {
+
+    public abstract void info(String msg, Object... args);
+    
+    public abstract void request(String msg, Object... args);
+
+    public abstract void response(String msg, Object... args);
+
+    public abstract void error(String message, Throwable t);
+
+    protected String getFormattedMessage(String message, Object... args) {
+        String msg = message.replace("{}", "%s");
+        return String.format(msg, args);
+    }
+
+}
